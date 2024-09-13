41b5533925a3907e7ffe0000a82c352a4f559ba7
ConnerWitTweeduizend <97093629@st.deltion.nl>
2024-09-13 12:55:55 +0200

    model en migration gemaakt voor content

---
+++ database/migrations/2024_09_13_093841_create_content_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('content', function (Blueprint $table) {
+            //tables for house model
+            $table->id();
+            $table->enum('type', ['house', 'apartment']);
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->enum('availability', ['available', 'not available']);
+            $table->decimal('price', 10, 2);
+            $table->longText('description')->nullable();
+            $table->integer('m2')->unsigned();
+            $table->integer('bedrooms')->unsigned();
+            $table->integer('bathrooms')->unsigned();
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('content');
+    }
+};
