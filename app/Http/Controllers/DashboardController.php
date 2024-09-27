cc1dca78a02bab8b9701abacd66602f4c00d7741
chaeriz <chrisobt@outlook.com>
2024-09-27 08:54:28 +0200

    change list function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,7 +12,6 @@ class DashboardController extends Controller
         // list all open apartements or houses that's connected to authenticated user
         $user = Auth::user();
         $content = Content::where('user_id', $user->id)
-                    ->where('availability', 'available')
                     ->get();
         if(!$content){return response()->json('No content found');};
         return response()->json($content);
