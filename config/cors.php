dfb1f917e36da8f144e3d1b88d7268b3dd1987de
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-10-14 12:56:58 +0200

    fix cors local

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'], // Adjust this to match your API routes
     'allowed_methods' => ['*'], // Allow all methods or specify which ones
-    'allowed_origins' => ['http://localhost:3000', 'https://chrisouboter.com', 'http://127.0.0.1:3000'], // Add the appropriate URLs
+    'allowed_origins' => ['*'], // Add the appropriate URLs
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], // Allow all headers or specify which ones
     'exposed_headers' => [],
